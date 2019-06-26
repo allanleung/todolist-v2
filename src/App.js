42e0966afc1f6969bda13efa8e956d1e933b7e05
Allan Leung <allanleung@Allans-MacBook-Pro.local>
2019-06-26 15:45:22 -0700

    Setup working

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import TodoItem from "./components/TodoItem";
 import TodoList from "./components/TodoList";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from 'uuid';
+
 class App extends Component {
     render() {
         return (
-            <div>
-                hello from app.js
-                <TodoItem/>
-                <TodoList/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-8 mt-4">
+                        <h3 className="text-capitalize text-center"> Todo Input</h3>
+                        <TodoItem/>
+                        <TodoList/>
+                    </div>
+
+                </div>
             </div>
         );
     }
